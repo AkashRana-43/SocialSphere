19d1801c99973eacdd82afafa46bd1709177f1ac
Akash Rana <cielo.rana113@gmail.com>
2024-03-06 13:30:17 +1030

    Navbar | Register | Login

---
+++ client/src/scenes/homePage/homePage.jsx
@@ -0,0 +1,15 @@
+import Box  from "@mui/material/Box";
+import Navbar from "scenes/navbar/navbar";
+
+const HomePage = () => {
+    return (
+    <Box>
+        <Navbar/>
+    </Box>
+    )
+};
+
+export default HomePage;
+
+
+
--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -16,6 +16,6 @@ export const verifyToken = async (req, res, next) => {
         req.user = verified;
         next();
     } catch (error) {
-        res.status(500).json({error: err.message});
+        res.status(500).json({error: error.message});
     }
 }
