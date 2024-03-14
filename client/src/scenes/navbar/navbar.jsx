4937601ec0a85db1abc1f7d6f46de0c958b7f72d
Akash Rana <cielo.rana113@gmail.com>
2024-03-14 19:13:40 +1030

    In Progress

--- client/src/scenes/navbar/navbar.jsx
+++ client/src/scenes/navbar/navbar.jsx
@@ -28,8 +28,7 @@ const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector((state) => state.auth.user);
-  console.log(user)
+  const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
   const theme = useTheme();
